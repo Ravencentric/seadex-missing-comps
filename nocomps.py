e8f253a8f1e3e4b30312eba287c9c98fbc614d50
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2024-12-19 00:42:15 +0530

    add more data

--- nocomps.py
+++ nocomps.py
@@ -8,8 +8,15 @@
 
 
 from seadex import SeaDexEntry
-from pyanilist import AniList
+from pyanilist import AniList, Media
 from time import sleep
+
+
+def sorter(entry: tuple[Media, str]) -> int:
+    """
+    Key function for sorting entries
+    """
+    return entry[0].popularity if entry[0].popularity is not None else -1
 
 
 def main() -> None:
@@ -17,22 +24,34 @@ def main() -> None:
         SeaDexEntry() as seadex_entry,
         AniList() as anilist,
     ):
-        entries: list[tuple[int, str, str]] = []
+        entries: list[tuple[Media, str]] = []
 
         for entry in seadex_entry.iterator():
-            if not any(comp.startswith("https://slow.pics") for comp in entry.comparisons):
+            if not any(
+                comp.startswith("https://slow.pics") for comp in entry.comparisons
+            ):
                 media = anilist.get_media(id=entry.anilist_id)
-                popularity = media.popularity if media.popularity is not None else -1
-                entries.append((popularity, str(media.title), entry.url))
+                entries.append((media, entry.url))
                 print(f"Found: {media.title} - {entry.url}")
-                sleep(2) # Avoid AniList rate limit
+                sleep(2)  # Avoid AniList rate limit
 
-        entries.sort(reverse=True, key=lambda x: x[0])
+        entries.sort(reverse=True, key=sorter)
 
         with open("README.md", "w", encoding="utf-8") as file:
             file.write("## SeaDex entries with no comparisons\n")
-            for idx, (_, title, url) in enumerate(entries, start=1):
-                file.write(f"{idx}. {title} - {url}\n")
+            file.write("0. Title / Year / Score / SeaDex / AniList\n")
+            for idx, (media, url) in enumerate(entries, start=1):
+                line = f"{idx}. {media.title}"
+
+                if year := media.start_date.year:
+                    line += f" / {year}"
+                if score := media.average_score:
+                    line += f" / {score}"
+
+                line += f" / [SeaDex]({url}) / [AniList]({media.site_url})\n"
+
+                file.write(line)
+
 
 if __name__ == "__main__":
     main()
